c53b3e7d2859a543fcd0093394093b09a220bb11
jvbeethoven <julievanbeethoven@gmail.com>
2018-01-28 13:41:24 -0500

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,70 +1,35 @@
-const path = require(`path`);
-const log = true;
+const Hapi = require('hapi');
+const inert = require('inert');
+const Path = require('path');
 
-require(`dotenv`).load({silent: true});
-
-const {
-  PORT = 3000,
-  URL = `http://localhost`,
-  MONGO_URL
-} = process.env;
-
-const Server = require(`hapi`).Server;
-
-const server = new Server({
-  connections: {
-    routes: {
-      files: {
-        relativeTo: path.join(__dirname, `public`)
-      }
+const server = new Hapi.Server({
+  port: 3000,
+  routes: {
+    files: {
+      relativeTo: Path.join(__dirname, 'public')
     }
   }
 });
 
-server.connection({port: PORT});
+const provision = async () => {
 
-server.start(err => {
+  await server.register(inert);
 
-  if (err) return console.error(err);
-
-  console.log(``);
-  console.log(`Server running at: ${URL}:${PORT}`);
-
-  server.register({
-
-    register: require(`hapi-devine-autoload`),
-
-    options: {
-
-      path: path.join(__dirname, `plugins`),
-      log,
-
-      plugins: [
-        `hapi-devine-mongodb`,
-        `hapi-devine-routes`,
-        `inert`
-      ],
-
-      pluginOptions: {
-
-        'hapi-devine-mongodb': {
-          connectionString: MONGO_URL,
-          log,
-          path: path.join(__dirname, `schemas`)
-        },
-
-        'hapi-devine-routes': {
-          log,
-          path: path.join(__dirname, `routes`),
-          after: 'hapi-devine-mongodb'
-        },
-
+  server.route({
+    method: `GET`,
+    path: `/{param*}`,
+    handler: {
+      directory: {
+        path: `.`,
+        redirectToSlash: true,
+        index: true,
       }
-
     }
-
-  }, error => {
-    if (error) return console.error(error);
   });
 
-});
+  await server.start();
+
+  console.log('Server running at:', server.info.uri);
+};
+
+provision();
