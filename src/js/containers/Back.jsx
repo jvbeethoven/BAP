19a4ea5fb61b57a22a366a765e52e97d772c3d33
jvbeethoven <julievanbeethoven@gmail.com>
2018-02-09 03:03:38 -0500

    Error message when submitting a wrong form

--- src/js/containers/Back.jsx
+++ src/js/containers/Back.jsx
@@ -13,7 +13,7 @@ const Back = ({store}) => {
   const {
     chosenDreams, sex, email, years, message, information, setEmail, setMessage, setYears, add, finalizeForm, backComplete
   } = store;
-  let $email, $message, $years, randomMessage;
+  let $email, $message, $years, $error, randomMessage;
   const randomMsg = chosenDreams[Math.floor(Math.random() * chosenDreams.length)];
 
   const containsChildren = item => {
@@ -42,6 +42,7 @@ const Back = ({store}) => {
       add(email, message, `test`, years, chosenDreams[0], chosenDreams[1], chosenDreams[2], chosenDreams[3], chosenDreams[4], sex);
       finalizeForm(true);
     } else {
+      $error.innerHTML = `Oeps, jouw toekomstige ik zal hier niet genoeg mee hebben. Vul het aantal jaren, jouw email adres en boodschap in.`;
       finalizeForm(false);
     }
   };
@@ -61,20 +62,24 @@ const Back = ({store}) => {
   return (
     <section className='card'>
         <form className='card-chosen' onSubmit={handleSubmit}>
+          <h1>Beste toekomstige ik,</h1>
           <div className='card-content'>
             <p>Binnen</p>
             <input type='number' ref={$el => $years = $el} onChange={handleYears} />
-            <p>jaar ben ik 35</p>
+            <p>jaar ben ik 35 en zou ik graag volgende dingen waargemaakt hebben:</p>
+            {chosenDreams.map(d => <p key={d}>{d}</p>)}
             <div className='card-content-chosenDreams'></div>
             <div className='card-content-addedText'>{randomMessage}</div>
           </div>
           <div className='card-user-info'>
             <img className='stamp'></img>
-            <label className='dreams-email-label'>E-mailadres</label>
+            <label className='dreams-email-label'>E-mailadres (we houden je toekomstige-ik hiermee op de hoogte!)</label>
             <input className='dreams-email-input' type='email' ref={$el => $email = $el} onChange={handleEmail} />
-            <label className='dreams-message-label'>Mijn toekomstboodschap</label>
+            <label className='dreams-message-label'>Wat zijn jouw volgende stappen om jouw dromen werkelijkheid te maken?</label>
             <input className='dreams-message-input' type='text' ref={$el => $message = $el} onChange={handleMessage} />
+            <h1>Groetjes, je vroegere ik.</h1>
             <input className='submit-dreams-form' type='submit' />
+            <p ref={$el => $error = $el}></p>
           </div>
         </form>
         {backComplete ? <Redirect to={`/`} /> : ``}
